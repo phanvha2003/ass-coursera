8a101ed22d343832885fefd9faded40639f96b41
pvha <phanvha2003@gmail.com>
2025-12-09 11:27:43 +0700

    Create CODE_OF_CONDUCT.m

---
+++ CODE_OF_CONDUCT.m
@@ -0,0 +1,21 @@
+# Code of Conduct
+
+## Our Pledge
+We pledge to make participation in our community a harassment-free experience for everyone. We are committed to creating a respectful and inclusive environment for contributors.
+
+## Our Standards
+Examples of behavior that contributes to a positive environment include:
+- Being respectful of differing viewpoints
+- Accepting constructive criticism
+- Showing empathy toward others
+
+Examples of unacceptable behavior include:
+- Harassment or derogatory comments
+- Publishing others’ private information
+- Any conduct that is inappropriate in a professional setting
+
+## Enforcement
+Instances of abusive or unacceptable behavior can be reported directly to the project maintainers. All complaints will be reviewed and handled appropriately.
+
+## Attribution
+This Code of Conduct is adapted from the Contributor Covenant, version 2.0.
